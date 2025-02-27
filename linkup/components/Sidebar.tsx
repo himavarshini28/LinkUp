7c18bb8a58a92bd117290beabb4fda17115ab30c
himavarshini28 <himavarshinimahapatruni@gmail.com>
2025-02-27 22:39:58 +0530

    sideBar

--- linkup/components/Sidebar.tsx
+++ linkup/components/Sidebar.tsx
@@ -1,11 +1,36 @@
+"use client"
+import Link from 'next/link'
 import React from 'react'
+import { sidebarLinks } from '@/constants'
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation'
 
 const Sidebar = () => {
+  const path=usePathname();
   return (
-    <div>
-      Sidebar
-    </div>
-  )
-}
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-col flex-1 gap-6">
+          {sidebarLinks.map((link)=>
+          {
+            const isActive = path===link.route ||  path.startsWith(`${link.route}/`);
 
-export default Sidebar
+            return (  
+            <Link
+              href={link.route}
+              key={link.label}
+              className={clsx('flex gap-4 items-center p-4 rounded-lg justify-start', { 'bg-blue-600': isActive })}
+            >
+              <img src={link.imgUrl}
+              alt={link.label}/>
+              <p>{link.label}</p>
+            </Link>);
+          })}
+      </div>
+
+    </section>
+  );
+};
+
+export default Sidebar;
+
+
---
+++ linkup/constants/index.ts
@@ -0,0 +1,28 @@
+export const sidebarLinks =
+[
+    {
+        label:'Home',
+        route:"/",
+        imgUrl:"/icons/home.svg",
+    },
+    {
+        label:'Previous',
+        route:"/previous",
+        imgUrl:"/icons/previous.svg",
+    },
+    {
+        label:'Upcoming',
+        route:"/upcoming",
+        imgUrl:"/icons/upcoming.svg",
+    },
+    {
+        label:'Personal Room',
+        route:"/personal-room",
+        imgUrl:"/icons/add-personal.svg",
+    },
+    {
+        label:'Recordings',
+        route:"/recordings",
+        imgUrl:"/icons/recordings.svg",
+    },
+]
